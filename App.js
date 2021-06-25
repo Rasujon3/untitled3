7f4dc56d3cfbb79656d53b16e8d032d679dfa488
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-06-26 01:14:55 +0600

    80 Responsive Alignself
    80 Responsive Alignself

--- App.js
+++ App.js
@@ -4,10 +4,9 @@ import {View, Text} from 'react-native';
 class App extends Component {
     render() {
         return (
-            <View style={{flex:100,alignItems:'flex-end',justifyContent:"space-evenly",flexDirection:'row'}}>
-                <View style={{width:"10%",height:"10%",backgroundColor:"red"}}></View>
-                <View style={{width:"10%",height:"10%",backgroundColor:"green"}}></View>
-                <View style={{width:"10%",height:"10%",backgroundColor:"yellow"}}></View>
+            <View style={{flex:100,flexDirection:'row'}}>
+                <View style={{alignSelf:'flex-end',width:"10%",height:"10%",backgroundColor:"red"}}></View>
+
             </View>
         );
     }
