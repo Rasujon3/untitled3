b478d9fc4177d8fdeb70db12430fd617e50b1173
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-06-26 01:08:52 +0600

    80 Responsive Alignment
    80 Responsive Alignment

--- App.js
+++ App.js
@@ -4,37 +4,10 @@ import {View, Text} from 'react-native';
 class App extends Component {
     render() {
         return (
-            <View style={{flex:100}}>
-                <View style={{backgroundColor:"red",flex:50}}>
-
-                  <View style={{backgroundColor:"white",flex:38}}></View>
-
-                  <View style={{flexDirection:"row",backgroundColor:"black",flex:12}}>
-                    <View style={{backgroundColor:"yellow",flex:4}}></View>
-                    <View style={{backgroundColor:"pink",flex:4}}></View>
-                    <View style={{backgroundColor:"red",flex:4}}></View>
-                  </View>
-
-                </View>
-
-
-              <View style={{backgroundColor:"black",flex:50}}>
-
-                <View style={{flexDirection:"row",backgroundColor:"black",flex:25}}>
-                  <View style={{backgroundColor:"green",flex:8}}></View>
-                  <View style={{backgroundColor:"black",flex:8}}></View>
-                  <View style={{backgroundColor:"teal",flex:8}}></View>
-                </View>
-
-                <View style={{flexDirection:"row",backgroundColor:"blue",flex:25}}>
-                  <View style={{backgroundColor:"red",flex:8}}></View>
-                  <View style={{backgroundColor:"green",flex:8}}></View>
-                  <View style={{backgroundColor:"yellow",flex:8}}></View>
-
-                </View>
-
-              </View>
-
+            <View style={{flex:100,alignItems:'flex-end',justifyContent:"space-evenly",flexDirection:'row'}}>
+                <View style={{width:"10%",height:"10%",backgroundColor:"red"}}></View>
+                <View style={{width:"10%",height:"10%",backgroundColor:"green"}}></View>
+                <View style={{width:"10%",height:"10%",backgroundColor:"yellow"}}></View>
             </View>
         );
     }
