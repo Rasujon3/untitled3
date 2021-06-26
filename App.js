e905ec7024689c062b503d1b705e1a03ef9d00b3
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-06-27 00:08:27 +0600

    89 Working With Rest API "Simple API call"
    89 Working With Rest API "Simple API call"

--- App.js
+++ App.js
@@ -1,97 +1,34 @@
 import React, {Component} from 'react';
-import {View, Text, Button, Alert, TextInput} from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
+import {View,Text,Button} from 'react-native';
 
 class App extends Component {
 
     constructor() {
         super();
-        this.state = {
-            AllData: '.........',
-
-            NameData: ' ',
-            RollData: ' ',
-            ClassData: ' ',
-        };
+        this.state={
+            SimpleData:"..."
+        }
     }
 
-    saveData = async () => {
+    CallMyFirstAPI=()=>{
+        let URL = "http://apishooter.com/getSimpleString";
+        let config = {Method:'GET'};
 
-        try {
-            var AllDataArray = [this.state.NameData, this.state.RollData, this.state.ClassData];
-            var AllDataArrayString = AllDataArray.toString();
-            await AsyncStorage.setItem('AllDataKey', AllDataArrayString);
-            Alert.alert('Data Saved');
-
-        } catch (e) {
-            Alert.alert('Data Save failed! Try again');
-        }
-
-    };
-
-    getData = async () => {
-
-        try {
-           var selectedData = await AsyncStorage.getItem('AllDataKey');
-            this.setState({AllData:selectedData});
-        } catch (e) {
-            Alert.alert('Data Get failed!');
-        }
-
-    };
-
-    deleteData = async () => {
-
-        try {
-            // await AsyncStorage.removeItem('nameKey');
-            await AsyncStorage.clear();
-            Alert.alert('Data Deleted Successfully!');
-        } catch (e) {
-            Alert.alert('Data Delete failed!');
-        }
-
-    };
+        fetch(URL,config)
+            .then((responseInitial)=>responseInitial.text())
+            .then((responseFinal)=>{
+                this.setState({SimpleData:responseFinal})
+            })
+            .catch((error)=>{
+                this.setState({SimpleData:"Server Error"})
+            })
+    }
 
     render() {
         return (
             <View>
-
-                <View style={{margin: 10}}>
-                    <TextInput onChangeText={(text) => {
-                        this.setState({NameData: text});
-                    }} style={{padding: 5, height: 40, borderColor: 'blue', borderWidth: 1}} placeholder="Your Name"/>
-                </View>
-
-                <View style={{margin: 10}}>
-                    <TextInput onChangeText={(text) => {
-                        this.setState({RollData: text});
-                    }} style={{padding: 5, height: 40, borderColor: 'blue', borderWidth: 1}} placeholder="Your Roll"/>
-                </View>
-
-                <View style={{margin: 10}}>
-                    <TextInput onChangeText={(text) => {
-                        this.setState({ClassData: text});
-                    }} style={{padding: 5, height: 40, borderColor: 'blue', borderWidth: 1}} placeholder="Your Class"/>
-                </View>
-
-                <View style={{margin: 10}}>
-                    <Button onPress={this.saveData} title="Save"/>
-                </View>
-
-                <View style={{margin: 10}}>
-                    <Button onPress={this.getData} title="Get"/>
-                </View>
-
-                <View style={{margin: 10}}>
-                    <Button onPress={this.deleteData} title="Delete"/>
-                </View>
-
-                <View style={{margin: 10}}>
-                    <Text style={{fontSize: 30}}>{this.state.AllData}</Text>
-                </View>
-
-
+                <Text style={{fontSize:40}}>{this.state.SimpleData}</Text>
+                <Button onPress={this.CallMyFirstAPI} title="Call"/>
             </View>
         );
     }
