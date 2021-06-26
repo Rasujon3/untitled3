ae91800ef618e2c3fafddcd355575f0e1204ea9c
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-06-27 01:08:19 +0600

    91 Set Loading Animation Between Req And Response
    91 Set Loading Animation Between Req And Response

--- App.js
+++ App.js
@@ -1,36 +1,50 @@
 import React, {Component} from 'react';
-import {View,Text,Button} from 'react-native';
+import {View, Text, Button, ActivityIndicator} from 'react-native';
 
 class App extends Component {
 
     constructor() {
         super();
-        this.state={
-            SimpleData:"..."
-        }
+        this.state = {
+            SimpleData: '...',
+            loading: false,
+        };
     }
 
-    CallMyFirstAPI=()=>{
-        let URL = "http://apishooter.com/getSimpleString";
-        let config = {Method:'GET'};
+    CallMyFirstAPI = () => {
+        this.setState({loading: true});
+        let URL = 'http://apishooter.com/getSimpleString';
+        let config = {Method: 'GET'};
 
-        fetch(URL,config)
-            .then((responseInitial)=>responseInitial.headers.get('date'))
-            .then((responseFinal)=>{
-                this.setState({SimpleData:responseFinal})
+        fetch(URL, config)
+            .then((responseInitial) => responseInitial.headers.get('cache-control'))
+            .then((responseFinal) => {
+                this.setState({SimpleData: responseFinal});
             })
-            .catch((error)=>{
-                this.setState({SimpleData:"Server Error"})
-            })
-    }
+            .catch((error) => {
+                this.setState({SimpleData: 'Server Error'});
+            }).finally(() => {
+            this.setState({loading: false});
+        });
+    };
 
     render() {
-        return (
-            <View>
-                <Text style={{fontSize:40}}>{this.state.SimpleData}</Text>
-                <Button onPress={this.CallMyFirstAPI} title="Call"/>
-            </View>
-        );
+        if (this.state.loading === true) {
+            return (
+                <View>
+                    <ActivityIndicator size="large" color="#ff2233"/>
+                </View>
+            );
+        } else {
+            return (
+                <View>
+                    <Text style={{fontSize: 40}}>{this.state.SimpleData}</Text>
+                    <Button onPress={this.CallMyFirstAPI} title="Call"/>
+
+                </View>
+            );
+        }
+
     }
 }
 
